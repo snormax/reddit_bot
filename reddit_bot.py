8983f0cb9fd9e238d4f89064c6701c2e991319e6
snormax <53067950+snormax@users.noreply.github.com>
2020-09-29 23:51:29 -0700

    draft

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,55 +1,57 @@
 import praw
 import time
 
-# Enter your correct Reddit information into the variable below
-userAgent = 'Enter Bot name'
-cID = 'Enter your personal use script'
-cSC = 'Enter you client secret'
-userN = 'Enter your Reddit username'
-userP = 'Enter your Reddit password'
-numFound = 0
-reddit = praw.Reddit(user_agent=userAgent, client_id=cID, client_secret=cSC, username=userN, password=userP)
 
-subreddit = reddit.subreddit('weather')  # any subreddit you want to monitor
+def read_from_file() -> list:
+    """
+    Extracts data from posts_replied_to.txt, filters, and returns the data as a list
+    :return: list from file
+    """
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))  # removes ""
+        return posts_replied_to
 
-bot_phrase = 'Aw shucks, looks like I am staying in >:('  # phrase that the bot replies with
 
-keywords = {'Cold', 'chicago', 'polar', 'vortex'}  # makes a set of keywords to find in subreddits
+# TODO find a time to do this in do_thing()
+def print_to_file(posts_replied_to: list):
+    """
+    Dumps current posts_replied_to to file to save it
+    :param posts_replied_to: current list of posts replied to
+    :return: None
+    """
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
 
-for submission in subreddit.hot(limit=10):  # this views the top 10 posts in that subbreddit
-    n_title = submission.title.lower()  # makes the post title lowercase so we can compare our keywords with it.
-    for i in keywords:  # goes through our keywords
-        if i in n_title:  # if one of our keywords matches a title in the top 10 of the subreddit
-            numFound = numFound + 1
-            print('Bot replying to: ')  # replies and outputs to the command line
-            print("Title: ", submission.title)
-            print("Text: ", submission.selftext)
-            print("Score: ", submission.score)
-            print("---------------------------------")
-            print('Bot saying: ', bot_phrase)
-            print()
-            submission.reply(bot_phrase)
 
-if numFound == 0:
-    print()
-    print("Sorry, didn't find any posts with those keywords, try again!")
+def init_bot():
+    pass
 
-# ANOTHER BOT -----------------------------------
 
-r = reddit
-r.login()
+def do_thing():
+    pass
 
-already_done = []
-prawWords = ['praw', 'reddit_api', 'mellort']
 
-while True:
-    subreddit = r.get_subreddit('learnpython')
-    for submission in subreddit.get_hot(limit=10):
-        op_text = submission.selftext.lower()
-        has_praw = any(string in op_text for string in prawWords)
-        # Test if it contains a PRAW-related question
-        if submission.id not in already_done and has_praw:
-            msg = '[PRAW related thread](%s)' % submission.short_link
-            r.send_message('_Daimon_', 'PRAW Thread', msg)
-            already_done.append(submission.id)
-    time.sleep(1800)
+# TODO break this up
+def main():
+    # Initializing bot
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit('bot subreddit')  # TODO make bot subreddit
+    comments = subreddit.stream.comments()  # Alternatively subreddit.get_comments(subreddit)
+
+    posts_replied_to = read_from_file()
+
+    # Checking for username mentions, if responded to, and then doing the thing
+    while True:
+        for comment in comments:
+            text = comment.body
+            if 'u/botname' in text.lower() and comment.id not in posts_replied_to:  # TODO bot name or !thing
+                comment.reply('hello')  # TODO do thing
+                posts_replied_to.append(comment.id)
+        time.sleep(3600)  # hour
+
+
+if __name__ == '__main__':
+    main()
