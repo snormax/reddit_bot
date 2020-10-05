317377a046861011eaeedc93538048a92721321f
snormax <53067950+snormax@users.noreply.github.com>
2020-10-05 16:31:06 -0700

    Heroku test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
 import praw
 import time
-from os import environ
+import os
+
 WAIT_TIME = 500
 CHALLENGE_TEXT = " to compete in 'Rock, Paper, Scissors.' Both have up to 5 MINUTES to private message " \
                  "u/rps_duel_bot with ONLY 'Rock', 'Paper', OR 'Scissors' in the SUBJECT field or the match will be " \
@@ -54,96 +56,130 @@ def parse_body(body: str):
     # Check username_list
     for username in username_list:
         if username != 'u/rps_duel_bot' and username is not None:
-            return username[2:]     # Up to 3 if /u/
+            return username[2:]  # Up to 3 if /u/
     return "-1"
 
 
-def check_messages():
-    # TODO
-    pass
+def check_mentions(reddit):
+    """
+    Checks for valid mentions, initiates match, checks messages
+    :param reddit: Reddit object
+    """
+    for mention in reddit.inbox.mentions():
+        if mention.new:
+            print("Checking a mention...")
+            mention.mark_read()
+            user_a = mention.author.name
+
+            # Parse and check for user_b
+            user_b = parse_body(mention.body)
+            if user_b == '-1' or user_b == user_a:
+                continue
+            print("UserA: %s, UserB %s" % (user_a, user_b))
+
+            # Send challenge reply
+            reply_text = "u/" + user_a + " has challenged u/" + user_b + CHALLENGE_TEXT
+            mention.reply(reply_text)
+
+            # For five minutes check unread messages, avoiding 'username mention'
+            rps_tuple = check_messages(reddit, user_a, user_b)
+            user_a_rps = rps_tuple[0]
+            user_b_rps = rps_tuple[1]
+
+            # Reply with decision
+            print("user_a_rps: %s, user_b_rps: %s" % (user_a_rps, user_b_rps))
+            if user_b_rps is None or user_a_rps is None:
+                mention.reply("This match is invalid due rule violation(s)")
+            elif user_a_rps == user_b_rps:
+                mention.reply("Draw!")
+            else:
+                winner = rps(user_a, user_a_rps, user_b, user_b_rps)
+                winner_reply = "u/" + winner + " is the winner!"
+                mention.reply(winner_reply)
 
 
-def check_mentions():
+def check_messages(reddit, user_a: str, user_b: str):
+    """
+    Sets a timer, continuously checks messages (until either timer or RPS choices), and returns the RPS choices
+
+    :param reddit: Reddit object
+    :param user_a: author
+    :param user_b: challengee
+    :return: RPS chosen by users and/or None
+    """
     # TODO
-    pass
+    start_time = time.time()
+    stop_time = start_time + WAIT_TIME
+    user_a_rps = None
+    user_b_rps = None
+
+    print("Start Time: %s, Stop Time: %s" % (start_time, stop_time))
+
+    while True:
+        current_time = time.time()
+        if current_time >= stop_time or (user_a_rps is not None and user_b_rps is not None):
+            break
+        for message in reddit.inbox.unread():
+            # Check if message is old: /message/ may be older than /mention/
+            if message.created_utc < start_time:
+                message.mark_read()
+                continue
+
+            # Subject parsing
+            elif user_a_rps is None and message.author.name == user_a:
+                subject = message.subject.lower()
+                if subject in VALID_INPUT:
+                    user_a_rps = subject.lower()
+            elif user_b_rps is None and message.author.name == user_b:
+                subject = message.subject.lower()
+                if subject in VALID_INPUT:
+                    user_b_rps = subject.lower()
+
+    print("User_A chose: %s, User_B chose: %s" % (user_a_rps, user_b_rps))
+    return user_a_rps, user_b_rps
+
+
+def log_in():
+    """
+    For credential security:
+    Testing can be done using locally stored credentials (not hosted to GitHub)
+    Heroku configuration file set with credentials as well
+    :return: Reddit object
+    """
+    # True if testing locally
+    testing = True
+
+    if not testing:
+        REDDIT_CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
+        REDDIT_CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
+        REDDIT_USERNAME = os.environ['REDDIT_USERNAME']
+        REDDIT_PASSWORD = os.environ['REDDIT_PASSWORD']
+        REDDIT_USER_AGENT = os.environ['REDDIT_USER_AGENT']
+
+    else:
+        credentials = open("credentials.json", "r")
+        credentials_json = json.load(credentials)
+        credentials.close()
+
+        REDDIT_CLIENT_ID = credentials_json['data']['REDDIT_CLIENT_ID']
+        REDDIT_CLIENT_SECRET = credentials_json['data']['REDDIT_CLIENT_SECRET']
+        REDDIT_USERNAME = credentials_json['data']['REDDIT_USERNAME']
+        REDDIT_PASSWORD = credentials_json['data']['REDDIT_PASSWORD']
+        REDDIT_USER_AGENT = credentials_json['data']['REDDIT_USER_AGENT']
+
+    return praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET,
+                       password=REDDIT_PASSWORD, username=REDDIT_USERNAME, user_agent=REDDIT_USER_AGENT)
 
 
 def main():
-    # Log-in
-    REDDIT_CLIENT_ID = environ['REDDIT_CLIENT_ID']
-    REDDIT_CLIENT_SECRET = environ['REDDIT_CLIENT_SECRET']
-    REDDIT_USERNAME = environ['REDDIT_USERNAME']
-    REDDIT_PASSWORD = environ['REDDIT_PASSWORD']
-    REDDIT_USER_AGENT = environ['REDDIT_USER_AGENT']
-    reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET,
-                         password=REDDIT_PASSWORD, username=REDDIT_USERNAME, user_agent=REDDIT_USER_AGENT)
-
-    # Check if logged-in correctly
-    print(reddit.user.me())
+    # Log-in and test
+    reddit = log_in()
+    print("Logged in as: %s" % (reddit.user.me()))
 
     while True:
         # Downtime precaution
         try:
-            for mention in reddit.inbox.mentions():
-                if mention.new:
-                    print("Checking message...")
-                    mention.mark_read()
-                    start_time = time.time()
-                    end_time = start_time + WAIT_TIME
-                    user_a = mention.author.name
-                    user_a_rps = None
-                    user_b_rps = None
-
-                    # Parse and check for user_b
-                    user_b = parse_body(mention.body)
-                    if user_b == '-1' or user_b == user_a:
-                        continue
-
-                    print("Start Time: %s, End Time: %s, UserA: %s, UserB %s" % (start_time, end_time, user_a, user_b))
-
-                    # Send challenge reply
-                    reply_text = "u/" + user_a + " has challenged u/" + user_b + CHALLENGE_TEXT
-                    mention.reply(reply_text)
-
-                    # For five minutes check unread messages, avoiding 'username mention',
-                    while True:
-                        current_time = time.time()
-                        if current_time >= end_time or (user_a_rps is not None and user_b_rps is not None):
-                            break
-                        for message in reddit.inbox.unread():
-                            # Checks if username mention and if so does NOT mark as read
-                            if message.subject.lower() == 'username mention':
-                                continue
-
-                            # Get this over with
-                            message.mark_read()
-
-                            # Old mail check
-                            if message.created_utc < start_time:
-                                continue
-
-                            # Subject check
-                            elif user_a_rps is None and message.author.name == user_a:
-                                subject = message.subject.lower()
-                                if subject in VALID_INPUT:
-                                    user_a_rps = subject
-                            elif user_b_rps is None and message.author.name == user_b:
-                                subject = message.subject.lower()
-                                if subject in VALID_INPUT:
-                                    user_b_rps = subject
-                                # NOT elif continue -- give them time for another attempt
-
-                    # Reply with decision
-                    print("user_a_rps: %s, user_b_rps: %s" % (user_a_rps, user_b_rps))
-                    if user_b_rps is None or user_a_rps is None:
-                        mention.reply("This match is invalid due rule violation(s)")
-                    elif user_a_rps == user_b_rps:
-                        mention.reply("Draw!")
-                    else:
-                        winner = rps(user_a, user_a_rps, user_b, user_b_rps)
-                        winner_reply = "u/" + winner + " is the winner!"
-                        mention.reply(winner_reply)
-
+            check_messages(reddit)
         except Exception as e:
             print(e)
             time.sleep(60)
